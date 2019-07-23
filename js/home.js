bc64d21f51d125329faf0dbfdf890c380f9dfb78
Raed Ouerfelli <werfelli.raed@yahoo.fr>
2019-07-23 12:05:11 +0200

    final changes for home page

--- js/home.js
+++ js/home.js
@@ -20,4 +20,29 @@ function changeBorderStyleNav() {
 $(function () {
     $(".text-about-me h2").addClass("animation-text"); // text animation in the header will starts only if the dom is ready
     changeBorderStyleNav();
+
+
+
 })
+
+$(document).ready(function(e) {
+//var windowBottom = $(window).height();
+    var index=0;
+    $(document).scroll(function(){
+        var top = $('.technical').height()-$(window).scrollTop();
+        console.log(top)
+        if(top <= (-2100)){
+            if(index==0){
+
+                $('.chart').easyPieChart({
+                    easing: 'easeOutBounce',
+                    onStep: function(from, to, percent) {
+                        $(this.el).find('.percent').text(Math.round(percent));
+                    }
+                });
+
+            }
+            index++;
+        }
+    })
+});
